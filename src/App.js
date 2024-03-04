4ecc5281c62b468a0a2a80fbe94ab3d8bc6ebb07
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-03-04 18:41:12 +0200

    add clarifai api to frontend

--- src/App.js
+++ src/App.js
@@ -3,20 +3,56 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'c6ba98ba70ed4bcb8afc8dd1dea6d90d'
+ });
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+        'face-detection', 
+        this.state.input
+      ).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg className="particles-bg-canvas-self" type="circle" />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
